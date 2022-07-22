775282f720a1a92ec48e97db7c2137421b0e3a46
Eduardy <eduardylopes@gmail.com>
2022-07-22 01:26:28 -0300

    connect app with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_MONGODB_URI_ACCESS),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
